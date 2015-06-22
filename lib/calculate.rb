014e761d551db8310800deb446650097dc391d3e
Michael <michaelrobert.fs@gmail.com>
2015-06-22 19:07:23 -0400

    Second commit with add and subtract

---
+++ lib/calculate.rb
@@ -0,0 +1,18 @@
+class Calculate
+	def add(x, y)
+		return x + y
+	end
+
+	def subtract(x, y)
+		return x - y
+	end
+
+	def multiple(x, y)
+		return x * y
+	end
+
+	def divide(x, y)
+		return x / y
+	end
+
+end
--- server.rb
+++ server.rb
@@ -1,8 +1,23 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
+require_relative 'lib/calculate.rb'
 
-get "/" do 
+total = Calculate.new
 
-erb :calculator
 
+get "/calculator" do 
+	erb :calculator
+end
+
+post "/calculator" do
+	@user_input_1 = params[:user_input_1].to_i
+	@user_input_2 = params[:user_input_2].to_i
+	@operation = params[:operation]
+
+	@total_add = total.add(@user_input_1, @user_input_2)
+	@total_sub = total.subtract(@user_input_1, @user_input_2)
+	# @total_mul = total.multiply(@user_input_1, @user_input_2)
+	# @total_div = total.divide(@user_input_1, @user_input_2)
+
+	erb :total
 end
